fcba8beacb8e6a3de0c3c04ccfd108a50585c6bb
NiMiTZ-code <niemiecdawid69@gmail.com>
2022-11-17 19:35:12 +0100

    Pierwsze pliki i szkielet projektu

---
+++ vue_weather_dashboard/src/components/TempVarChart.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="temvarchart">
+        <p>Informacje o temperaturze:</p>
+        {{tempVar}}
+    </div>
+</template>
+<script>
+export default {
+    props: ["tempVar"],
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    },
+}
+</script>
---
+++ vue_weather_dashboard/src/components/UVIndex.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="uvindex">
+      <p>Index UV: {{ uvindex }}</p>
+    </div>
+  </template>
+  
+  <script>
+  
+  export default {
+    props: ["highlights"],
+    data () {
+      return {
+        uvindex: this.highlights.uvindex
+      }
+    },
+    methods: {
+  
+    },
+    computed: {
+  
+    },
+  }
+  </script>
+  
+  <style>
+  
+  </style>
+  
