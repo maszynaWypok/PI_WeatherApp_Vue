b164fc43bdd7801a733a2f40da03740cd3725034
NiMiTZ-code <niemiecdawid69@gmail.com>
2022-12-28 14:34:43 +0100

    Fix of styling incorporation, vue template adjusted

--- vue_weather_dashboard/src/App.vue
+++ vue_weather_dashboard/src/App.vue
@@ -1,64 +1,82 @@
 <template>
-  <div id="app">
-    <dashboard-content :highlights="highlights" :tempVar="tempVar"></dashboard-content>
-    <div id="search">
-      <input id="location-input" type="text" ref="input" placeholder="Twoja lokacja?" @keyup.enter="organizeAllDetails">
-      <button id="search-btn" @click="organizeAllDetails"><img src=".\assets\Search.svg" width="24" height="24"></button>
-    </div>
-    <div id="info">
-      <div class="wrapper-left">
-        <div id="current-weather">
-          {{ currentWeather.temp }}
-            <span>°C</span>
-        </div>
-        <div id="weather-desc">{{ currentWeather.summary }}</div>
-          <div class="temp-max-min">
-            <div class="max-desc">
-              <div id="max-detail">
-                <i>▲</i>
-                {{ currentWeather.todayHighLow.todayTempHigh }}
+  <div id="ancestor">
+    <div class="container-fluid" id="app">
+      <div class="row">
+        <div id="sidebar" class="col-md-3 col-sm-4 col-xs-12 sidebar">
+          <div id="search">
+            <input
+              id="location-input"
+              type="text"
+              ref="input"
+              placeholder="Location?"
+              @keyup.enter="organizeAllDetails"
+            >
+            <button id="search-btn" @click="organizeAllDetails">
+              <img src="./assets/Search.svg" width="24" height="24">
+            </button>
+          </div>
+          <div id="info">
+            <div class="wrapper-left">
+              <div id="current-weather">
+                {{ currentWeather.temp }}
                 <span>°C</span>
               </div>
-            <div id="max-summary">at {{ currentWeather.todayHighLow.todayTempHighTime }}</div>
+              <div id="weather-desc">{{ currentWeather.summary }}</div>
+              <div class="temp-max-min">
+                <div class="max-desc">
+                  <div id="max-detail">
+                    <i>▲</i>
+                    {{ currentWeather.todayHighLow.todayTempHigh }}
+                    <span>°C</span>
+                  </div>
+                  <div id="max-summary">at {{ currentWeather.todayHighLow.todayTempHighTime }}</div>
+                </div>
+                <div class="min-desc">
+                  <div id="min-detail">
+                    <i>▼</i>
+                    {{ currentWeather.todayHighLow.todayTempLow }}
+                    <span>°C</span>
+                  </div>
+                  <div id="min-summary">at {{ currentWeather.todayHighLow.todayTempLowTime }}</div>
+                </div>
+              </div>
+            </div>
+            <div class="wrapper-right">
+              <div class="date-time-info">
+                <div id="date-desc">
+                  <img src="./assets/calendar.svg" width="20" height="20">
+                  {{ currentWeather.time }}
+                </div>
+              </div>
+              <div class="location-info">
+                <div id="location-desc">
+                  <img
+                    src="./assets/location.svg"
+                    width="10.83"
+                    height="15.83"
+                    style="opacity: 0.9;"
+                  >
+                  {{ currentWeather.full_location }}
+                  <div id="location-detail" class="mt-1">
+                    Lat: {{ currentWeather.formatted_lat }}
+                    <br>
+                    Long: {{ currentWeather.formatted_long }}
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-        <div class="min-desc">
-          <div id="min-detail">
-          <i>▼</i>
-          {{ currentWeather.todayHighLow.todayTempLow }}
-          <span>°C</span>
         </div>
-        <div id="min-summary">at {{ currentWeather.todayHighLow.todayTempLowTime }}</div>
+        <dashboard-content
+          class="col-md-9 col-sm-8 col-xs-12 content"
+          id="dashboard-content"
+          :highlights="highlights"
+          :tempVar="tempVar"
+        ></dashboard-content>
       </div>
     </div>
   </div>
-  <div class="wrapper-right">
-    <div class="date-time-info">
-      <div id="date-desc">
-        <img src="./assets/calendar.svg" width="20" height="20">
-        {{ currentWeather.time }}
-      </div>
-    </div>
-    <div class="location-info">
-      <div id="location-desc">
-        <img
-          src="./assets/location.svg"
-          width="10.83"
-          height="15.83"
-          style="opacity: 0.9;"
-        >
-        {{ currentWeather.full_location }}
-        <div id="location-detail" class="mt-1">
-          Szer: {{ currentWeather.formatted_lat }}
-          <br>
-          Dł: {{ currentWeather.formatted_long }}
-        </div>
-      </div>
-    </div>
-  </div>
-  </div>
-</div>
-
-</template>
+ </template>
 
 <script>
 import { computed } from 'vue';
