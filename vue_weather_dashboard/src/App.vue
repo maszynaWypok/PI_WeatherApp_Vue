48ef8bf1c41838287133fc5157d79e505b50d7d1
NiMiTZ-code <niemiecdawid69@gmail.com>
2022-12-28 11:28:05 +0100

    new .env file, added functions for data processing in App.vue

--- vue_weather_dashboard/src/App.vue
+++ vue_weather_dashboard/src/App.vue
@@ -117,7 +117,14 @@ export default {
     }
   },
   methods: {
-
+    //just utility stuff
+    convertToTitleCase: function(str) {
+     str = str.toLowerCase().split(' ');
+     for (var i = 0; i < str.length; i++) {
+       str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+     }
+     return str.join(' ');
+   },
     //action-methods
     makeInputEmpty: function() {
       this.$refs.input.value = '';
@@ -145,6 +152,139 @@ export default {
       this.makeInputEmpty();
       this.makeTempVarTodayEmpty();
     },
+
+    //api call
+    fixWeatherApi: function() {
+      var weatherApi = 'https://api.openweathermap.org/data/2.5/weather?q=' + this.location + '&appid=' + process.env.VUE_APP_OPEN_WEATHER_API_KEY + '&units=metric';
+      this.completeWeatherApi = weatherApi;
+    },
+    fetchWeatherData: async function() {
+      await this.fixWeatherApi();
+      var axios = require('axios');
+      var weatherApiResponse = await axios.get(this.completeWeatherApi);
+      if (weatherApiResponse.status === 200) {
+        this.rawWeatherData = weatherApiResponse.data;
+      } else {
+        alert("Houston, we don't have a forecast for you yet");
+      }
+    },
+    //methods for data processing
+    getTimezone: function() {
+     return this.rawWeatherData.timezone;
+   },
+   getSetCurrentTime: function() {
+     var currentTime = this.rawWeatherData.currently.time;
+     var timezone = this.getTimezone();
+     this.currentWeather.time = this.unixToHuman( //unix to human still to write
+       timezone,
+       currentTime
+     ).fullTime;
+   },
+   getSetSummary: function() {
+    var currentSummary = this.convertToTitleCase(
+      this.rawWeatherData.currently.summary
+    );
+    if (currentSummary.includes(' And')) {
+      currentSummary = currentSummary.replace (' And', ',');
+    }
+    this.currentWeather.summary = currentSummary;
+   },
+   getSetPossibility: function() {
+     var possible = this.formatPossibility(this.rawWeatherData.daily.icon);
+     if (possible.includes(' And')) {
+       possible = possible.replace(' And', ',');
+     }
+     this.currentWeather.possibility = possible;
+   },
+   getSetCurrentTemp: function() {
+     currentWeather.temp = this.rawWeatherData.currently.temperature;
+     
+   },
+   getTodayDetails: function() {
+     return this.rawWeatherData.daily.data[0];
+   },
+   getSetTodayTempHighLowWithTime: function() {
+     var timezone = this.getTimezone();
+     var todayDetails = this.getTodayDetails();
+     this.currentWeather.todayHighLow.todayTempHigh = todayDetails.temperatureMax;
+     
+     this.currentWeather.todayHighLow.todayTempHighTime = this.unixToHuman(
+       timezone,
+       todayDetails.temperatureMaxTime
+     ).onlyTime;
+     this.currentWeather.todayHighLow.todayTempLow = todayDetails.temperatureMin;
+     this.currentWeather.todayHighLow.todayTempLowTime = this.unixToHuman(
+       timezone,
+       todayDetails.temperatureMinTime
+     ).onlyTime;
+   },
+   getHourlyInfoToday: function() {
+     return this.rawWeatherData.hourly.data;
+   },
+   getSetHourlyTempInfoToday: function() {
+     var unixTime = this.rawWeatherData.currently.time;
+     var timezone = this.getTimezone();
+     var todayMonthDate = this.unixToHuman(timezone, unixTime).onlyMonthDate;
+     var hourlyData = this.getHourlyInfoToday();
+     for (var i = 0; i < hourlyData.length; i++) {
+       var hourlyTimeAllTypes = this.unixToHuman(timezone, hourlyData[i].time);
+       var hourlyOnlyTime = hourlyTimeAllTypes.onlyTime;
+       var hourlyMonthDate = hourlyTimeAllTypes.onlyMonthDate;
+       if (todayMonthDate === hourlyMonthDate) {
+         var hourlyObject = { hour: '', temp: '' };
+         hourlyObject.hour = hourlyOnlyTime;
+         hourlyObject.temp = this.fahToCel(hourlyData[i].temperature).toString();
+         this.tempVar.tempToday.push(hourlyObject);
+         /*
+         Since we are using array.push(), we are just adding elements
+         at the end of the array. Thus, the array is not getting emptied
+         first when a new location is entered.
+         to solve this problem, a method this.makeTempVarTodayEmpty()
+         has been created, and called from this.locationEntered().
+         */
+       }
+     }
+     /*
+     To cover the edge case where the local time is between 10 — 12 PM,
+     and therefore there are only two elements in the array
+     this.tempVar.tempToday. We need to add the points for minimum temperature
+     and maximum temperature so that the chart gets generated with atleast four points.
+     */
+     if (this.tempVar.tempToday.length <= 2) {
+       var minTempObject = {
+         hour: this.currentWeather.todayHighLow.todayTempHighTime,
+         temp: this.currentWeather.todayHighLow.todayTempHigh
+       };
+       var maxTempObject = {
+         hour: this.currentWeather.todayHighLow.todayTempLowTime,
+         temp: this.currentWeather.todayHighLow.todayTempLow
+       };
+       /*
+       Typically, lowest temp are at dawn,
+       highest temp is around mid day.
+       Thus we can safely arrange like min, max, temp after 10 PM.
+       */
+       // array.unshift() adds stuff at the beginning of the array.
+       // the order will be: min, max, 10 PM, 11 PM.
+       this.tempVar.tempToday.unshift(maxTempObject, minTempObject);
+     }
+   },
+   getSetUVIndex: function() {
+     var uvIndex = this.rawWeatherData.currently.uvIndex;
+     this.highlights.uvIndex = uvIndex;
+   },
+   getSetVisibility: function() {
+     this.highlights.visibility = this.rawWeatherData.currently.visibility;
+   },
+   getSetWindStatus: function() {
+     this.highlights.windStatus.windSpeed = this.rawWeatherData.currently.windSpeed;
+     var absoluteWindDir = this.rawWeatherData.currently.windBearing;
+     this.highlights.windStatus.windDirection = absoluteWindDir;
+     this.highlights.windStatus.derivedWindDirection = this.deriveWindDir(
+       absoluteWindDir
+     );
+   },
+
 
 
 
