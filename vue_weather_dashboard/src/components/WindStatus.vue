e75bdc0a8c9ee9870eed3b415aa21b089cdda6da
NiMiTZ-code <niemiecdawid69@gmail.com>
2023-01-02 13:10:59 +0100

    Added chart for wind (icons needed)

--- vue_weather_dashboard/src/components/WindStatus.vue
+++ vue_weather_dashboard/src/components/WindStatus.vue
@@ -1,31 +1,32 @@
 <template>
-    <div id="windStatus">
-      <p>Wiatr: {{ windSpeed}} m/s</p>
-      <p>Kierunek: {{ windDirectionDegrees }} stopni - {{windDirectionDir}}</p>
+  <div class="highlights-item col-md-4 col-sm-6 col-xs-12 border-top">
+    <div>
+    <div class="card-heading pt-5">Wind Status</div>
+    <div class="row pt-4 mt-4">
+      <div class="col-sm-6 col-md-6 mt-2 text-center align-middle">
+        <p class="card-sub-heading mt-3">Wind Direction</p>
+        <p class="mt-4"><img src="../assets/winddirection.svg" height="40" width="40"></p>
+        <p class="card-value mt-4">{{ highlights.windStatus.windDirectionDir }}</p>
+        <p class="card-value mt-4">{{ highlights.windStatus.windDirectionDegrees }}° </p>
+      </div>
+      <div class="col-sm-6 col-md-6 mt-2">
+        <p class="card-sub-heading mt-3">Wind Speed</p>
+        <p class="mt-4"><img src="../assets/windspeed.svg" height="40" width="40"></p>
+        <p class="card-value mt-4">{{ highlights.windStatus.windSpeed }} km/h</p>
+      </div>
     </div>
-  </template>
-  
-  <script>
-  
-  export default {
-    props: ["highlights"],
-    data () {
-      return {
-        windSpeed: this.highlights.windStatus.windSpeed,
-        windDirectionDegrees: this.highlights.windStatus.windDirectionDegrees,
-        windDirectionDir: this.highlights.windStatus.windDirectionDir
-      }
-    },
-    methods: {
-  
-    },
-    computed: {
-  
-    },
-  }
-  </script>
-  
-  <style>
-  
-  </style>
-  
+    </div>
+  </div>
+ </template>
+ 
+ <script>
+ export default {
+  props: ["highlights"],
+  components: {},
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {}
+ };
+ </script>
