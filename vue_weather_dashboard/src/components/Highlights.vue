a18ea464eb1439a42b6de179564a1b042b665aaa
NiMiTZ-code <niemiecdawid69@gmail.com>
2023-01-08 16:45:58 +0100

    code beautify

--- vue_weather_dashboard/src/components/Highlights.vue
+++ vue_weather_dashboard/src/components/Highlights.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="custom-content-card content-card card">
     <div class="card-body pb-0">
-    <div class="content-header h4 text-center pt-2 pb-3">Highlights</div>
+      <div class="content-header h4 text-center pt-2 pb-3">Highlights</div>
       <div class="row">
         <uv-index :highlights="highlights"></uv-index>
         <visibility :highlights="highlights"></visibility>
